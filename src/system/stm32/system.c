599bb52fe68f58e163e14a9eec5aba89cfa17c2e
delta-12 <btesq235@gmail.com>
2025-11-16 13:06:56 -0500

    random functions

--- src/system/stm32/system.c
+++ src/system/stm32/system.c
@@ -1,6 +1,33 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 
+#include "zenoh-pico/system/platform.h"
+
+extern uint32_t ZenohUser_GetRandomU32(void);
+
+/*------------------ Random ------------------*/
+uint8_t z_random_u8(void) { return (uint8_t)z_random_u32(); }
+
+uint16_t z_random_u16(void) { return (uint16_t)z_random_u32(); }
+
+uint32_t z_random_u32(void) { return ZenohUser_GetRandomU32(); }
+
+uint64_t z_random_u64(void) {
+    uint64_t value = 0;
+    value |= (uint64_t)z_random_u32();
+    value = value << 32U;
+    value |= (uint64_t)z_random_u32();
+    return value;
+}
+
+void z_random_fill(void *buf, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        *((uint8_t *)buf) = z_random_u8();
+    }
+}
+
+/*------------------ Memory ------------------*/
 /* TODO custom static memory allocator */
 
 void *z_malloc(size_t size) { return malloc(size); }
